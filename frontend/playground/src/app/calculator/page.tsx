20cab637ffe10968b29150a6729c698fd6842536
Choihyoungkyu <poi5971234@gmail.com>
2023-08-23 18:06:54 +0900

    [FE] 계산기 디자인 틀 완료 및 기본 로직 구성중

--- frontend/playground/src/app/calculator/page.tsx
+++ frontend/playground/src/app/calculator/page.tsx
@@ -5,22 +5,115 @@ import style from '@/styles/calculator.module.css';
 import { useState } from 'react';
 
 const calculator = () => {
-  const [currentNum, setCurrentNum] = useState<number>(0);
-  const handleButton = async (num: String) => {
-    console.log(num);
-    return;
+  const [buffer, setBuffer] = useState<number>(0);
+  const [num, setNum] = useState<number>(0);
+  const [previousSymbol, setPreviousSymbol] = useState<string>(null);
+  const [isPercentage, setIsPercentage] = useState<boolean>(false);
+
+  console.log('num :', num, 'buffer :', buffer, 'symbol :', previousSymbol);
+
+  const handleButton = async (value: string) => {
+    if (isNaN(parseInt(value))) {
+      handleSymbol(value);
+    } else {
+      handleNumber(value);
+    }
+  };
+
+  const handleSymbol = (symbol: string) => {
+    switch (symbol) {
+      case 'AC':
+        setBuffer(0);
+        setNum(0);
+        break;
+      case '=':
+        if (previousSymbol === null) {
+          return;
+        }
+        flushOperation();
+        setPreviousSymbol(null);
+        setBuffer(num);
+        setNum(0);
+        setIsPercentage(false);
+        break;
+      case '←':
+        if (buffer.toString().length === 1) {
+          setBuffer(0);
+        } else {
+          setBuffer((prev) => parseInt(prev.toString().substring(0, prev.toString().length)));
+        }
+        break;
+      case '+':
+      case '-':
+      case 'x':
+      case '÷':
+        handleMath(symbol);
+        break;
+    }
+  };
+
+  const handleMath = (symbol: string) => {
+    if (buffer === 0) {
+      return;
+    }
+
+    if (num === 0) {
+      setNum(buffer);
+    } else {
+      flushOperation();
+    }
+    setPreviousSymbol(symbol);
+    setBuffer(0);
+  };
+
+  const flushOperation = () => {
+    if (previousSymbol === '+') {
+      setNum((prev) => prev + buffer);
+    } else if (previousSymbol === '-') {
+      setNum((prev) => prev - buffer);
+    } else if (previousSymbol === 'x') {
+      setNum((prev) => prev * buffer);
+    } else if (previousSymbol === '÷') {
+      setNum((prev) => prev / buffer);
+    }
+  };
+
+  const handleNumber = (value: string) => {
+    if (buffer === 0) {
+      setBuffer(parseInt(value));
+    } else {
+      setBuffer((prev) => parseInt(prev.toString() + value));
+    }
+  };
+
+  const doPlusMinus = () => {
+    if (num > 0) {
+      setNum((prev) => parseInt('-' + prev.toString()));
+    } else if (num < 0) {
+      setNum((prev) => parseInt(prev.toString().substring(1, prev.toString().length)));
+    }
+  };
+
+  const doPercent = () => {
+    if (!isPercentage) {
+      setNum((prev) => prev * 0.01);
+      setIsPercentage(true);
+    } else {
+      setNum((prev) => prev * 100);
+      setIsPercentage(false);
+    }
   };
 
   return (
     <div className={style.wrap}>
       <section>
-        <div className={style.num}>{currentNum}</div>
+        <div className={style.num}>{buffer}</div>
       </section>
       <section>
         <div className={style.numRow}>
           <NumButton num={'AC'} handleButton={handleButton} />
-          <NumButton num={'+/-'} handleButton={handleButton} />
-          <NumButton num={'%'} handleButton={handleButton} />
+          <NumButton num={'+/-'} handleButton={doPlusMinus} />
+          <NumButton num={'%'} handleButton={doPercent} />
           <NumButton num={'÷'} handleButton={handleButton} />
         </div>
         <div className={style.numRow}>
--- frontend/playground/src/components/NumButton/NumButton.tsx
+++ frontend/playground/src/components/NumButton/NumButton.tsx
@@ -4,11 +4,12 @@ import React from 'react';
 import style from './NumButton.module.css';
 
 interface PropsType {
-  num: String;
-  handleButton: (num: String) => void;
+  num: string;
+  handleButton: (num: string) => void;
+  className?: string | null;
 }
 
-const NumButton = ({ num, handleButton }: PropsType) => {
+const NumButton = ({ num, handleButton, className }: PropsType) => {
   return (
     <button className={style.button} onClick={() => handleButton(num)}>
       {num}
